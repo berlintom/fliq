ec68f57c7fff025bef3c41ebea40943ae26db65e
Teresa Finsterwald <teresa.finsterwald@gmail.com>
2021-11-23 14:27:04 +0100

    create participation(not paths yet

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -3,4 +3,23 @@ class ParticipationsController < ApplicationController
     @match = Match.find(params[:match_id])
     @participation = Participation.new
   end
+
+  def create
+    @participation = Participation.new(params_participation)
+    @match = Match.find(params[:match_id])
+    @participation.match = @match
+    @participation.user = current_user
+    @participation.save
+    if @participation.save
+      redirect_to
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_participation
+    params.require(:participation).permit(:status, :team)
+  end
 end
