1bf571f86350eea51195e901159d4909f2232da3
Valerie Finsterwald <finsterwald.valerie@gmail.com>
2021-11-23 16:39:07 +0100

    basicstyle

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -23,16 +23,18 @@ class MatchesController < ApplicationController
     end
   end
 
+  def mymatches
+    @participations = Participation.where(user: current_user)
+    @matches = Match.where(user: current_user)
+  end
+
   private
 
  def params_match
   params.require(:match).permit(:capacity, :start_date, :end_date, :venue_id)
   end
 
-  def mymatches
-    @participations = Participation.where(user: current_user)
-    @matches = Match.where(user: current_user)
-  end
+
 
   def accept
   end
