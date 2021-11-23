ad68923c07a942299bafc7aa9a4301c45f9dff53
Teresa Finsterwald <teresa.finsterwald@gmail.com>
2021-11-23 15:18:18 +0100

    create participation

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,20 +1,12 @@
 class ParticipationsController < ApplicationController
-  def new
-    @match = Match.find(params[:match_id])
-    @participation = Participation.new
-  end
 
   def create
-    @participation = Participation.new(params_participation)
+    @participation = Participation.new(status: "pending", team: nil)
     @match = Match.find(params[:match_id])
     @participation.match = @match
     @participation.user = current_user
     @participation.save
-    if @participation.save
-      redirect_to
-    else
-      render :new
-    end
+    redirect_to mymatches_path
   end
 
   private
