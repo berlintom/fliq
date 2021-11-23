3305ecd383f54aaf7472a780a596343a5c1dd26f
Egor Dranev <egor.dranev@gmail.com>
2021-11-23 12:52:36 +0100

    user can offer a match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,22 @@
 class MatchesController < ApplicationController
+  def new
+    @match = Match.new
+  end
+
+  def create
+    @match = Match.new(params_match)
+    @match.user = current_user
+    @match.save
+    if @match.save
+      redirect_to matches_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+ def params_match
+  params.require(:match).permit(:capacity, :start_date, :end_date)
+  end
 end
