f54893fe318f3b97a400cf4b48d1c411a7676558
Thomas Kreutz <thomas.kreutz89@gmail.com>
2021-11-23 17:35:49 +0100

    created mapbox

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,15 @@
 class VenuesController < ApplicationController
 
+  def index
+    @venues = Venue.all
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @venues.geocoded.map do |venue|
+      {
+        lat: venue.latitude,
+        lng: venue.longitude
+      }
+    end
+  end
   # to be edited
   # def venue_params
   #   params.require(:venue).permit(:title, :body, photos: [])
---
+++ app/javascript/controllers/mapbox_controller.js
@@ -0,0 +1,47 @@
+// app/javascript/controllers/mapbox_controller.js
+import { Controller } from "@hotwired/stimulus";
+import mapboxgl from 'mapbox-gl';
+
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue;
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+  }
+}
+
+// app/javascript/controllers/mapbox_controller.js
+connect() {
+  // [...]
+  this._addMarkersToMap();
+}
+
+_addMarkersToMap() {
+  this.markersValue.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(this.map);
+  });
+}
+
+// app/javascript/controllers/mapbox_controller.js
+connect() {
+  // [...]
+  this._addMarkersToMap();
+  this._fitMapToMarkers();
+}
+
+// [...]
+_fitMapToMarkers() {
+  const bounds = new mapboxgl.LngLatBounds();
+  this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+  this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+}
