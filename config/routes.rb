586459aca1582993bb43d8c52fc1f093aa4586d2
Thomas Kreutz <thomas.kreutz89@gmail.com>
2021-11-23 17:41:45 +0100

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :matches, only: [:index, :show, :new, :create] do
     resources :participations, only: [:new, :create]
   end
+
+  resources :venues, only: [:index]
 
   patch "/participations/:id/accepted", to: "participations#accept", as: :participation_accept
   patch "/participations/:id/declined", to: "participations#decline", as: :participation_decline
