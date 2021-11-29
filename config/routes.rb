de89824dc8258d72266d9406394ad0a74f8aeaf5
Teresa Finsterwald <teresa.finsterwald@gmail.com>
2021-11-29 10:47:11 +0100

    messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
 
-  resources :chatrooms, only: :show
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
 end
