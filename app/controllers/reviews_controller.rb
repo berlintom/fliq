eb7310c9fd11c32e626079e84d495a7b25018c3a
Thomas Kreutz <thomas.kreutz89@gmail.com>
2021-11-30 12:41:44 +0100

    review v1 + point sum render in user profile

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,37 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+  end
+
 end
+#   def create
+#     @review = Review.new(review_params)
+#     @review.match = @restaurant
+#     # @review.restaurant_id = @restaurant.id
+#     if @review.save
+#       redirect_to match_path(@restaurant)
+#     else
+#       render :new
+#     end
+#   end
+
+#   def destroy
+#     raise
+#     @review = Review.find(params[:id])
+#     @restaurant = @review.restaurant
+#     @review.destroy # JUST an sql query to delete a row in DB:  DELETE FROM REVIEWS WHERE ID = review.id
+#     redirect_to restaurant_path(@restaurant)
+#   end
+
+#   private
+
+#   def set_match
+#     @match = Match.find(params[:match_id])
+#   end
+
+#   def review_params
+#     params.require(:review).permit(:content)
+#   end
+# end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :participations, only: [:new, :create]
     resources :scores, only: :create
     resources :messages, only: :create
+    resources :reviews, only: [:new, :create]
   end
 
   resources :venues, only: [:index, :show]
