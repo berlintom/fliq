07cced671f9c35abaae6f8e08080a2bdfa312622
Thomas Kreutz <thomas.kreutz89@gmail.com>
2021-11-30 14:28:28 +0100

    view form v1

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,14 @@
 class ReviewsController < ApplicationController
-  before_action :set_restaurant
+  before_action :set_match
 
   def new
     @review = Review.new
   end
 
    def create
-    @match = Match.new(review_params)
-    @review.match = @restaurant
+    @review = Review.new(review_params)
+    raise
+    @review.match = @match
     if @review.save
       redirect_to match_path(@match)
     else
@@ -31,37 +32,4 @@ class ReviewsController < ApplicationController
    def review_params
     params.require(:review).permit(:comment, :rating)
   end
-
 end
-
-
-#   def create
-#     @review = Review.new(review_params)
-#     @review.match = @restaurant
-#     # @review.restaurant_id = @restaurant.id
-#     if @review.save
-#       redirect_to match_path(@restaurant)
-#     else
-#       render :new
-#     end
-#   end
-
-#   def destroy
-#     raise
-#     @review = Review.find(params[:id])
-#     @restaurant = @review.restaurant
-#     @review.destroy # JUST an sql query to delete a row in DB:  DELETE FROM REVIEWS WHERE ID = review.id
-#     redirect_to restaurant_path(@restaurant)
-#   end
-
-#   private
-
-#   def set_match
-#     @match = Match.find(params[:match_id])
-#   end
-
-#   def review_params
-#     params.require(:review).permit(:content)
-#   end
-# end
-# end
