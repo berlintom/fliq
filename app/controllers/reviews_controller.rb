0eab117f4a1b1d288b811c34b02089117507d748
Thomas Kreutz <thomas.kreutz89@gmail.com>
2021-11-30 14:16:10 +0100

    review routes and controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,40 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant
 
   def new
     @review = Review.new
   end
 
+   def create
+    @match = Match.new(review_params)
+    @review.match = @restaurant
+    if @review.save
+      redirect_to match_path(@match)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @match = @review.match
+    @review.destroy
+    redirect_to match_path(@match)
+  end
+
+  private
+
+  def set_match
+    @match = Match.find(params[:match_id])
+  end
+
+   def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+
 end
+
+
 #   def create
 #     @review = Review.new(review_params)
 #     @review.match = @restaurant
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
 
 
   resources :users, only: [:show, :index]
-
+  resources :reviews, only: [:destroy]
 end
