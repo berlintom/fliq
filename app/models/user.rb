240850a5a0346768fafb50560b7ef7b942ff250d
Egor Dranev <egor.dranev@gmail.com>
2021-11-30 16:51:00 +0100

    phone number validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,12 @@ class User < ApplicationRecord
   has_many :reviews
   delegate :point_sum, to: :profile
 
-  validates :first_name, :user_name, :phone_number, presence: true
+  validates :first_name, :user_name, :phone_number, :email, presence: true
   validates :phone_number, :user_name, uniqueness: true
+
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+  validates :phone_number, format: { with: /(?:\+?|\b)[0-9]{10}\b/ }
+  # validates :email, format: { with: /\A.*@.*\.com\z/, message: "Email should be valid!" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
