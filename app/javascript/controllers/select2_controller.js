0a3d990956d54f13fef429578dda83403ecd870e
Egor Dranev <egor.dranev@gmail.com>
2021-12-01 14:31:38 +0100

    venue search function in match create forme

---
+++ app/javascript/controllers/select2_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "stimulus"
+import $ from "jquery"
+import "select2"
+
+export default class extends Controller {
+
+  connect(){
+
+    $(this.element).select2();
+
+   }
+}
